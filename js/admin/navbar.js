cd36cc987de04b7c531ad25ebda3a6bd971777c2
Verry R. Wibawa <verryrwibawa@gmail.com>
2022-10-22 11:48:41 +0700

    fix mobile nav

--- js/admin/navbar.js
+++ js/admin/navbar.js
@@ -1,4 +1,5 @@
 const navToggler = document.querySelector(".navbar-toggler");
+const sidebar = document.querySelector(".sidebar");
 
 // show sidebar on nav toggler click
 navToggler.addEventListener("click", function () {
